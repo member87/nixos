a697c14aae956d36174282bf641707235df18f45
Jack <43416057+member87@users.noreply.github.com>
2025-04-30 19:57:38 +0100

    update

--- hosts/common/base/services/ssh.nix
+++ hosts/common/base/services/ssh.nix
@@ -1,12 +1,4 @@
-{ pkgs, ... }:
-{
-  services.openssh = {
-    enable = false;
-    settings = {
-      PasswordAuthentication = false;
-    };
-  };
-
+{pkgs, ...}: {
   programs.ssh = {
     enableAskPassword = true;
     askPassword = "${pkgs.lxqt.lxqt-openssh-askpass}/bin/lxqt-openssh-askpass";
