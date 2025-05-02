4b72bbc16668000a4ca0b4c038616871a89da3a1
Jack <43416057+member87@users.noreply.github.com>
2025-05-02 20:24:03 +0100

    update

--- hosts/frigg/boot.nix
+++ hosts/frigg/boot.nix
@@ -11,4 +11,38 @@
       enable = false;
     }
   ];
+
+  boot = {
+    initrd.systemd.enable = true;
+    initrd.availableKernelModules = [
+      "nvme"
+      "ahci"
+      "xhci_pci"
+      "thunderbolt"
+      "usbhid"
+      "sd_mod"
+      "v4l2loopback"
+    ];
+
+    kernelModules = ["kvm-amd"];
+
+    extraModulePackages = [
+      pkgs.unstable.linuxPackages_zen.v4l2loopback
+    ];
+
+    kernelPackages = pkgs.unstable.linuxPackages_zen;
+
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
+    };
+  };
 }
