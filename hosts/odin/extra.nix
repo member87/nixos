c8176539d883c4d4c40bcf3626c9ae6c670846f4
Jack <43416057+member87@users.noreply.github.com>
2025-05-06 11:52:03 +0100

    update

--- hosts/odin/extra.nix
+++ hosts/odin/extra.nix
@@ -136,7 +136,7 @@
 
     settings = {
       default_session = {
-        command = ''            
+        command = ''                  
           ${pkgs.greetd.tuigreet}/bin/tuigreet \
             --remember \
             --remember-session \
--- hosts/odin/hardware.nix
+++ hosts/odin/hardware.nix
@@ -8,15 +8,18 @@
   pkgs,
   modulesPath,
   ...
-}:
-
-{
+}: {
   imports = [
     inputs.disko.nixosModules.disko
-    (import ./disks.nix { inherit lib; })
+    (import ./disks.nix {inherit lib;})
   ];
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
