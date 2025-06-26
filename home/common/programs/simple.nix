e2bd1421e42f017f7cded1afe88e3bdf5f4cbbd8
Jack <43416057+member87@users.noreply.github.com>
2025-06-26 18:56:48 +0100

    move mangohud

--- home/common/programs/simple.nix
+++ home/common/programs/simple.nix
@@ -8,25 +8,25 @@
       enable = true;
       enableZshIntegration = true;
     };
+
+    mangohud = {
+      enable = true;
+      enableSessionWide = true;
+
+      settings = {
+        hud_compact = true;
+        position = "top-left";
+        frame_timing = false;
+        frametime = false;
+        hud_no_margin = true;
+        width = 80;
+        table_columns = 2;
+        font_size = 20;
+      };
+    };
   };
 
   services = {
     dunst.enable = true;
-  };
-
-  programs.mangohud = {
-    enable = true;
-    enableSessionWide = true;
-
-    settings = {
-      hud_compact = true;
-      position = "top-left";
-      frame_timing = false;
-      frametime = false;
-      hud_no_margin = true;
-      width = 80;
-      table_columns = 2;
-      font_size = 20;
-    };
   };
 }
