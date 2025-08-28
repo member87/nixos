67486fdaeed9a075abfb716af868ed54b593cd2b
Jack <43416057+member87@users.noreply.github.com>
2025-08-28 23:39:35 +0100

    autoswitch pipewire profiles

--- hosts/common/base/services/pipewire.nix
+++ hosts/common/base/services/pipewire.nix
@@ -1,8 +1,13 @@
-{ ... }:
-{
+{...}: {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = true;
+      };
+    };
   };
 }
