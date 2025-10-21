f16457db46bffab0e044a4b822406e50823f1ab7
Jack <43416057+member87@users.noreply.github.com>
2025-10-21 15:57:42 +0100

    update zillij binds

--- home/common/programs/zellij.nix
+++ home/common/programs/zellij.nix
@@ -26,12 +26,18 @@ _: {
         bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
         bind "Alt j" "Alt Down" { MoveFocus "Down"; }
         bind "Alt k" "Alt Up" { MoveFocus "Up"; }
+        bind "Ctrl Alt h" { Resize "Left"; }
+        bind "Ctrl Alt l" { Resize "Right"; }
+        bind "Ctrl Alt j" { Resize "Down"; }
+        bind "Ctrl Alt k" { Resize "Up"; }
         bind "Ctrl t" { NewTab; }
         bind "Ctrl w" { CloseTab; }
         bind "Ctrl n" { NewPane; }
         bind "Ctrl x" { CloseFocus; }
       }
     }
+
+    show_startup_tips false
   '';
 
   xdg.configFile."zellij/layouts/default.kdl".text = ''
@@ -43,34 +49,6 @@ _: {
         pane
         pane size=1 borderless=true {
           plugin location="status-bar"
-        }
-      }
-    }
-  '';
-
-  xdg.configFile."zellij/layouts/code.kdl".text = ''
-    layout {
-      default_tab_template {
-        pane size=1 borderless=true {
-          plugin location="tab-bar"
-        }
-        children
-        pane size=1 borderless=true {
-          plugin location="status-bar"
-        }
-      }
-      tab name="editor" split_direction="vertical" {
-        pane edit="." {
-            focus true
-        }
-      }
-      tab name="terminal" split_direction="vertical" {
-        pane command="npm" name="dev server" {
-          args "run" "dev"
-        }
-        pane size="40%" split_direction="horizontal" {
-            pane
-            pane
         }
       }
     }
