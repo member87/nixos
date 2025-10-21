92c43093da977b587d18b118fb5739eb2f2c779e
Jack <43416057+member87@users.noreply.github.com>
2025-10-21 16:01:52 +0100

    update git config

--- home/common/programs/git.nix
+++ home/common/programs/git.nix
@@ -6,13 +6,15 @@
   programs.git = {
     enable = true;
 
-    userName = "Jack";
-    userEmail = "43416057+member87@users.noreply.github.com";
-
-    extraConfig = {
+    settings = {
       commit.gpgsign = true;
       gpg.format = "ssh";
       user.signingkey = "~/.ssh/id_ed25519.pub";
+
+      user = {
+        email = "43416057+member87@users.noreply.github.com";
+        name = "Jack";
+      };
     };
   };
 }
