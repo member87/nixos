4f4ebdf3fc1fd875ad9c5927e1bb8397d4ae7be0
Jack <43416057+member87@users.noreply.github.com>
2025-11-11 14:59:24 +0000

    eww updates

--- home/common/desktop/hypridle.nix
+++ home/common/desktop/hypridle.nix
@@ -1,24 +1,37 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: let
+  agsEnabled = config.programs.ags.enable or false;
+  agsQuitCmd =
+    if agsEnabled
+    then "killall gjs; "
+    else "";
+  agsRunCmd =
+    if agsEnabled
+    then "; ags run"
+    else "";
+in {
   services.hypridle = {
     enable = true;
 
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        before_sleep_cmd = "${agsQuitCmd}loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on${agsRunCmd}";
       };
 
       listener = [
         {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "${agsQuitCmd}loginctl lock-session";
         }
         {
           timeout = 1200;
           on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          on-resume = "hyprctl dispatch dpms on${agsRunCmd}";
         }
         {
           timeout = 1800;
